2dae2741fd1a8a437255c161785ad544f2adb704
Profe-Julio <102832270+Profe-Julio@users.noreply.github.com>
2022-04-01 14:39:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("Mi primer app")
 st.button("Click aquí")
-st.image("https://github.com/Profe-Julio/PrimerRepositorio/blob/main/Homer.jpg")
+st.ballons()
+## st.image("https://github.com/Profe-Julio/PrimerRepositorio/blob/main/Homer.jpg")
